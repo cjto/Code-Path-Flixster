7646467d99ead7d0723898fab110533073ee7302
cyrejorin <cyrejorin@users.noreply.github.com>
2020-01-23 15:00:51 -0800

    Initial Commit

---
+++ Code Path flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Path flixster
+//
+//  Created by Cyre Jorin To on 1/23/20.
+//  Copyright © 2020 Cyre Jorin To. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Code Path flixsterTests/Code_Path_flixsterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Code_Path_flixsterTests.swift
+//  Code Path flixsterTests
+//
+//  Created by Cyre Jorin To on 1/23/20.
+//  Copyright © 2020 Cyre Jorin To. All rights reserved.
+//
+
+import XCTest
+@testable import Code_Path_flixster
+
+class Code_Path_flixsterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
